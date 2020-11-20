6e4676138cde2a4cd5d88ec6ea526204fa0cb71d
sgarg5858 <sgarg5858@gmail.com>
2020-11-20 12:05:25 +0530

    reviews -section in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CustomHeaderComponent } from './custom-header/custom-header.component';
 import { CustomSectionComponent } from './custom-section/custom-section.component';
 import { AboutSectionComponent } from './about-section/about-section.component';
+import { ReviewsSectionComponent } from './reviews-section/reviews-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomHeaderComponent,
     CustomSectionComponent,
-    AboutSectionComponent
+    AboutSectionComponent,
+    ReviewsSectionComponent
   ],
   imports: [
     BrowserModule,
